c4a478d030f181e5124db402f3767dc189416a35
Mike Capps <michaelcapps1772@yahoo.com>
2017-06-05 20:05:49 -0400

    Add Random Quote Function and Random Color

--- js/script.js
+++ js/script.js
@@ -1,4 +1,75 @@
+// Random Quote array
+var quotes = [
+    {
+        quote: "You can do anything but not everything.",
+        source: "David Allen",
+        citation: "Making It All Work",
+        year: 2009
+    },
+    {
+        quote: "You can question somebody's views and their judgement without questioning their motives or patriotism.",
+        source: "Barack Obama",
+        citation: "University of Michigan Commencement",
+        year: 2010
+    },
+    {
+        quote: "For three days after death hair and fingernails continue to grow but phone calls taper off.",
+        source: "Johnny Carson",
+    },
+    {
+        quote: "The frangrance always remains in the hand that gives the rose.",
+        source: "Heda Bejar",
+    },
+    {
+        quote: "Fear of a name increases fear of the thing itself.",
+        source: "J. K. Rowling",
+        citation: "Harry Potter and the Sorcerer's Stone",
+        year: 1997
+    }
+];
+
+// Color array for random background color
+var colors = ['#33FFBD', '#C0392B', '#F4D03F', '#17202A', '#154360', '#A569BD', '#3498DB'];
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
+document.addEventListener('DOMContentLoaded', printQuote, false);
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
+
+// Prints the random quote from the getRandomQuote function to the quote box
+function printQuote() {
+    var quote = getRandomQuote();
+    var color = getRandomColor();
+    document.body.style.backgroundColor = color;
+    document.getElementById('loadQuote').style.backgroundColor = color;
+    var html = '<p class="quote">' + quote.quote + '</p><p class="source">' + quote.source;
+    
+    if (quote.hasOwnProperty('citation')){
+        html += '<span class="citation">' + quote.citation + '</span>';
+    }
+    
+    if (quote.hasOwnProperty('year')){
+        html += '<span class="year">' + quote.year + '</span>';
+    }
+    
+    html += '</p>';
+    document.getElementById('quote-box').innerHTML = html;
+    
+}
+
+// Selects a random quote from the quotes array
+function getRandomQuote() {
+    
+    var len = quotes.length;
+    var random = (Math.round(Math.random() * (len - 1)));
+    return quotes[random];
+    
+    
+}
+
+// Selects a random color from the colors array (for random background color when new quote appears)
+function getRandomColor() {
+    var random = (Math.round(Math.random() * (colors.length -1)));
+    return colors[random];
+}
